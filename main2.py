020a101671751b014701f6c911f718b4b9d42e79
abenfraj <72936702+abenfraj@users.noreply.github.com>
2022-04-11 16:32:44 +0200

    mouse wheel event almost fully implemented

--- main2.py
+++ main2.py
@@ -1,10 +1,8 @@
 import pathlib
-import numpy as np
 from PIL import Image
 from PIL.Image import Resampling
-from PIL.ImageQt import ImageQt
-from PyQt5 import uic, QtGui, QtWidgets, QtCore
-from PyQt5.QtGui import QPixmap
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap, QWheelEvent
 from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication, QPushButton, QLabel
 
 # To prevent the following error: "Image size (460000000 pixels) exceeds limit of 178956970 pixels, could be decompression bomb DOS attack"
@@ -19,6 +17,8 @@ class UI(QMainWindow):
         super(UI, self).__init__()
 
         # Initialize the UI component variables
+        self.mousetracker_label = None
+        self.image_is_displayed = False
         self.bitmap_image = None
         self.bitmap_data = None
         self.save_bmp_data_button = None
@@ -27,10 +27,12 @@ class UI(QMainWindow):
         self.quit_button = None
         self.bitmap_label = None
 
+        self.scale = 1.0
+
         uic.loadUi(str(pathlib.Path(__file__).parent.resolve()) + "\\main.ui", self)  # Load the UI file
         self.retranslateUi()  # Translate the UI components into Python Objects
         self.connectAllWidgets()  # Connect all the UI components to their respective functions
-
+        self.setMouseTracking(True)  # Enable mouse tracking
         self.show()  # Show the main window
 
     # Function to translate the main window's UI components into Python Objects
@@ -40,6 +42,7 @@ class UI(QMainWindow):
         self.select_bitmap_button = self.findChild(QPushButton, "select_bitmap_button")
         self.bitmap_label = self.findChild(QLabel, "bitmap_label")
         self.save_bmp_data_button = self.findChild(QPushButton, "save_bmp_data_button")
+        self.mousetracker_label = self.findChild(QLabel, "mousetracker_label")
 
     # Function to connect all the UI components to their respective functions
     # @param self The object pointer
@@ -72,6 +75,7 @@ class UI(QMainWindow):
             self.pixmap = QPixmap("_resized.bmp")
             self.bitmap_label.setPixmap(self.pixmap)  # Set the bitmap label's pixmap to the QPixmap object
             self.bitmap_image.close()
+            self.image_is_displayed = True
 
     # Save the bitmap data to a file
     # @param self The object pointer
@@ -92,11 +96,37 @@ class UI(QMainWindow):
                     file.write(str_iteration)
             file.close()
 
-            #  ''.join(str(print(self.bitmap_data[j][i])) for i in range(self.image_height)
-            #  for j in range(self.image_width)))  # Write the bitmap data to the file
+    def mouseMoveEvent(self, e):
+        # TODO : static values, have to make it dynamic
+        x = e.x() - 22
+        y = e.y() - 82
+        text = f'x: {x},  y: {y}'
+        if x < 0 or y < 0 or x >= self.bitmap_label.width() or y >= self.bitmap_label.height() or not self.image_is_displayed:
+            self.mousetracker_label.setHidden(True)
+        else:
+            self.mousetracker_label.setHidden(False)
+            self.mousetracker_label.setText(text)
 
-            # text = self.textEdit.toPlainText()
-            # file.write(text)
+    def wheelEvent(self, event: QWheelEvent):
+        if self.bitmap_label is not None:
+            if event.angleDelta().y() > 0:
+                self.on_zoom_in(event)
+                # self.resize_image()
+            else:
+                self.on_zoom_out(event)
+                # self.resize_image()
+
+    def on_zoom_in(self, event):
+        self.scale *= 2
+
+    def on_zoom_out(self, event):
+        self.scale /= 2
+
+    # TODO : have to fix this
+    def resize_image(self):
+        size = self.pixmap.size()
+        scaled_pixmap = self.pixmap.scaled(self.scale * size)
+        self.label.setPixmap(scaled_pixmap)
 
 
 if __name__ == "__main__":
